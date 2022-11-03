b8945f5d55f6b0ec94397093a71de475dbb79043
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2022-11-03 11:48:26 -0500

    RevisiónRapida1

--- Assets/Interfaz Grafica/Scripts/ScenManager.cs
+++ Assets/Interfaz Grafica/Scripts/ScenManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ScenManager : MonoBehaviour
 {
+    public string nombreS;
     // Start is called before the first frame update
     void Start()
     {
